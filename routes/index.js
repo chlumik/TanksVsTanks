6f20e50e092b1d2e5e06a22a33e8af1ec0c752f6
Jiří Chlum <chlum.jiri@gmail.com>
2016-05-31 16:25:40 +0200

    add new page before enter

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Vítejte' });
 });
+
+router.get('/game', function (req, res, next) {
+  res.render('game', {title: 'Tanks vs Tanks'})
+})
 
 module.exports = router;
